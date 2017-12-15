fd61e710c5513548f8b7a368266637015c6410e9
ggdek <discoggdek@gmail.com>
2017-12-15 15:40:29 +0100

    2

--- blog/forms.py
+++ blog/forms.py
@@ -4,9 +4,9 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('titolo', 'testo',)
+        fields = ('title', 'text',)
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('autore', 'testo',)
+        fields = ('author', 'text',)
