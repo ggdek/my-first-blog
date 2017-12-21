31ec883c8498d40e7b23499e75d1446697b5c740
ggdek <discoggdek@gmail.com>
2017-12-21 08:32:22 +0100

    uffa

--- blog/models.py
+++ blog/models.py
@@ -29,5 +29,3 @@ class Comment(models.Model):
         self.save()
     def __str__(self):
         return self.text
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
